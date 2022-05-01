c2a58e598bd47ad777cc9caecdcb855ae04becd4
rayhan-dev <mohammodrayhan444@gmail.com>
2022-04-30 21:41:58 -0700

    registration form created

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -6,29 +6,37 @@ const Login = () => {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
   return (
-      <div className="my-5">
-          <h1 className="py-4">Login to your Account</h1>
+    <div className="my-5">
+      <h1 className="py-4">Login to your Account</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
           placeholder="Enter Your Emial"
           className="w-25 h-10 form_input d-flex flex-col mx-auto mb-3"
           {...register("email")}
+          required
         />
         <input
           placeholder="Pasword"
           className="w-25 h-10 form_input d-flex flex-col mx-auto mb-3"
           {...register("password")}
+          required
         />
         <input
           className="login_btn d-flex flex-col mx-auto mb-2"
           type="submit"
         />
-          </form>
-          <h6>New On BookIpedia ?</h6>
-          <Link className="fw-bold" style={{
-              "color": "orangered",
-                "textDecoration":"none"
-          }} to={'/register'}>Register Now!</Link>
+      </form>
+      <h6>New On BookIpedia ?</h6>
+      <Link
+        className="fw-bold"
+        style={{
+          color: "orangered",
+          textDecoration: "none",
+        }}
+        to={"/register"}
+      >
+        Register Now!
+      </Link>
     </div>
   );
 };
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <div>
-            <h1>this is register form</h1>
-            
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div className="py-5">
+      <h1 className="py-3">Please Sign Up</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="Last Name"
+          className="w-25 h-10 form_input d-flex flex-col mx-auto mb-3"
+          {...register("name")}
+          required
+        />
+        <input
+          placeholder="Enter Your Emial"
+          className="w-25 h-10 form_input d-flex flex-col mx-auto mb-3"
+          {...register("email")}
+          required
+        />
+        <input
+          placeholder="Pasword"
+          className="w-25 h-10 form_input d-flex flex-col mx-auto mb-3"
+          {...register("password")}
+          required
+        />
+        <input
+          className="login_btn d-flex flex-col mx-auto mb-2"
+          type="submit"
+        />
+          </form>
+          <h6 className="pt-3">Already Have an account?</h6>
+      <Link
+        className="fw-bold"
+        style={{
+          color: "orangered",
+          textDecoration: "none",
+        }}
+        to={"/login"}
+      >
+        Log in to your account!
+      </Link>
+    </div>
+  );
 };
 
 export default Register;
