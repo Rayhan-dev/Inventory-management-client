68f4d576a8935d9c672a78f4d626c324fa86b3d2
Rayhan-dev <mohammodrayhan444@gmail.com>
2022-05-02 12:10:53 -0700

    device changed

--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -14,6 +14,7 @@ const Header = () => {
             <Nav className="ms-auto">
               <Nav.Link className="navbar_item" as={Link} to={'/'}>Home</Nav.Link>
               <Nav.Link as={Link} to={'/login'} className="navbar_item" >Login</Nav.Link>
+              <Nav.Link as={Link} to={'/login'} className="navbar_item" >Login</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
