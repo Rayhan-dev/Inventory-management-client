c9ad91930a169b413b3d0a304858c6cf91da785d
Rayhan-dev <mohammodrayhan444@gmail.com>
2022-05-08 01:41:25 +0600

    env file created

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCC9yNN1i7fHgQ3iY1zbTc8Bz-IzhzC90g",
-  authDomain: "warehouse-management-31966.firebaseapp.com",
-  projectId: "warehouse-management-31966",
-  storageBucket: "warehouse-management-31966.appspot.com",
-  messagingSenderId: "818743099058",
-  appId: "1:818743099058:web:da7f71bfd168e56a1413ab"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
